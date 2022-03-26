d85c0b3a182c8eccc73de3d23c5976e7a1797d1f
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2022-03-27 02:58:52 +0700

    add UserService

--- src/main/java/com/tirmizee/services/UserService.java
+++ src/main/java/com/tirmizee/services/UserService.java
@@ -1,2 +1,27 @@
-package com.tirmizee.services;public class UserService {
+package com.tirmizee.services;
+
+import com.tirmizee.transfers.*;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    public UserResponse getById(Long id) {
+        return null;
+    }
+
+    public UserResponse getByUsername(String username) {
+        return null;
+    }
+
+    public UserUpdateResponse update(UserUpdateRequest request) {
+        return null;
+    }
+
+    public UserCreateResponse create(UserCreateRequest request) {
+        return null;
+    }
+
 }
