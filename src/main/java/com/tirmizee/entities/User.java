020d7d93b240bbf2805c5f4ed3d7f123816e3f83
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2022-03-27 02:57:37 +0700

    add User entity

--- src/main/java/com/tirmizee/entities/User.java
+++ src/main/java/com/tirmizee/entities/User.java
@@ -1,2 +1,20 @@
-package com.tirmizee.entities;public class User {
+package com.tirmizee.entities;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+
+    @Id
+    private Long id;
+    private String username;
+    private String email;
+
 }
