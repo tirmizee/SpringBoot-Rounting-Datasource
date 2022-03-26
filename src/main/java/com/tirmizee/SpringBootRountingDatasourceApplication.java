eb5a1c1f71bf378c3489caf46d54e9e474cb16a4
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2022-03-26 20:49:32 +0700

    init project

---
+++ src/main/java/com/tirmizee/SpringBootRountingDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.tirmizee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRountingDatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRountingDatasourceApplication.class, args);
+	}
+
+}
